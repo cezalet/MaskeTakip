cadb73c0ae25b13ac647cc3c0335ca007482d49d
bekir <bekir@OTOMASYON>
2024-02-07 15:06:15 +0300

    Add project files.

---
+++ MaskeTakip/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MaskeTakip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mesaj = "Merhaba";
+            double tutar = 10000;
+            int sayi = 100;
+            bool girisYapmısMi=false;
+
+            Console.WriteLine(mesaj);
+            Console.WriteLine(mesaj);
+            Console.WriteLine(mesaj);
+            Console.WriteLine(mesaj);
+            Console.WriteLine(mesaj);
+            Console.WriteLine(mesaj);
+
+            Console.WriteLine(tutar * 1.18);
+            Console.WriteLine(tutar * 1.18);
+
+            Console.ReadLine();
+        }
+    }
+}
